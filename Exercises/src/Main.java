bdf595189348cf90251f579f54caf4b614599118
gilberto <gilbertoeb@utexas.edu>
2025-01-31 23:48:06 -0600

    Adding Main class.

---
+++ Exercises/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main(String[] args) {
+
+    System.out.println("Hello, World!");
+    testing();
+  }
+
+  public static void testing() {
+
+    //        TwoSum twoSum = new TwoSum();
+    //        twoSum.testing();
+
+    ValidParenthesis_E validParenthesis = new ValidParenthesis_E();
+    validParenthesis.testing();
+  }
+}
