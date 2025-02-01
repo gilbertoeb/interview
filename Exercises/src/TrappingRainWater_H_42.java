2072ebc96fcad0f1f81505f89d148be6df02a77e
gilberto <gilbertoeb@utexas.edu>
2025-01-31 23:35:53 -0600

    Adding implementation of TrappingRainWater_H_42.

--- Exercises/src/TrappingRainWater_H_42.java
+++ Exercises/src/TrappingRainWater_H_42.java
@@ -19,43 +19,64 @@ n == height.length
  */
 public class TrappingRainWater_H_42 {
 
-    // Skeleton method where the implementation should go
-    public int trap(int[] height) {
-        // TODO: Implement the algorithm to calculate trapped rain water
-        return 0;
+  // Skeleton method where the implementation should go
+  public int trap(int[] height) {
+    int length = height.length;
+
+    int trappedWater = 0;
+
+    int left = 0;
+    int right = length - 1;
+
+    int maxLeft = height[left];
+    int maxRight = height[right];
+
+    while (left < right) {
+      if (maxLeft < maxRight) {
+        left++;
+        maxLeft = Math.max(maxLeft, height[left]);
+        trappedWater += maxLeft - height[left];
+      } else {
+        right--;
+        maxRight = Math.max(maxRight, height[right]);
+        trappedWater += maxRight - height[right];
+      }
     }
 
-    // Testing method with detailed outputs and comments
-    public void testing() {
-        int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int[] height2 = {4, 2, 0, 3, 2, 5};
-        int[] height3 = {1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
-        int[] height4 = {2, 0, 2};
+    return trappedWater;
+  }
 
-        System.out.println("Test Case 1: height = [0,1,0,2,1,0,1,3,2,1,2,1]");
-        System.out.println("Expected Output: 6");
-        System.out.println("Actual Output: " + trap(height1));
-        System.out.println();
+  // Testing method with detailed outputs and comments
+  public void testing() {
+    int[] height1 = {0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int[] height2 = {4, 2, 0, 3, 2, 5};
+    int[] height3 = {1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1};
+    int[] height4 = {2, 0, 2};
 
-        System.out.println("Test Case 2: height = [4,2,0,3,2,5]");
-        System.out.println("Expected Output: 9");
-        System.out.println("Actual Output: " + trap(height2));
-        System.out.println();
+    System.out.println("Test Case 1: height = [0,1,0,2,1,0,1,3,2,1,2,1]");
+    System.out.println("Expected Output: 6");
+    System.out.println("Actual Output: " + trap(height1));
+    System.out.println();
 
-        System.out.println("Test Case 3: height = [1,0,2,1,0,1,3,2,1,2,1]");
-        System.out.println("Expected Output: 6");
-        System.out.println("Actual Output: " + trap(height3));
-        System.out.println();
+    System.out.println("Test Case 2: height = [4,2,0,3,2,5]");
+    System.out.println("Expected Output: 9");
+    System.out.println("Actual Output: " + trap(height2));
+    System.out.println();
 
-        System.out.println("Test Case 4: height = [2,0,2]");
-        System.out.println("Expected Output: 2");
-        System.out.println("Actual Output: " + trap(height4));
-        System.out.println();
-    }
+    System.out.println("Test Case 3: height = [1,0,2,1,0,1,3,2,1,2,1]");
+    System.out.println("Expected Output: 6");
+    System.out.println("Actual Output: " + trap(height3));
+    System.out.println();
 
-    // Main method
-    public static void main(String[] args) {
-        TrappingRainWater_H_42 solution = new TrappingRainWater_H_42();
-        solution.testing();
-    }
+    System.out.println("Test Case 4: height = [2,0,2]");
+    System.out.println("Expected Output: 2");
+    System.out.println("Actual Output: " + trap(height4));
+    System.out.println();
+  }
+
+  // Main method
+  public static void main(String[] args) {
+    TrappingRainWater_H_42 solution = new TrappingRainWater_H_42();
+    solution.testing();
+  }
 }
